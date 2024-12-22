7ab88aeaa9c1d8e4e577826f78826a5c21df423b
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-12-22 20:57:08 +0100

    cleanup

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,8 +1,6 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
-import Image from 'next/image';
 
 
 export default async function ProtectedPage() {
@@ -30,10 +28,6 @@ export default async function ProtectedPage() {
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           {JSON.stringify(user, null, 2)}
         </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
       </div>
     </div>
   );
