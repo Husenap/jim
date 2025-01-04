39a902bd61d4777a13fb736dc6f049ebe3150964
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2025-01-04 18:09:09 +0100

    updated page container

--- components/page-container.tsx
+++ components/page-container.tsx
@@ -4,13 +4,15 @@ export default function PageContainer({
   children,
   topNavbar,
   bottomNavbar,
+  disableViewTransitions,
 }: {
   children?: React.ReactNode;
   topNavbar?: React.ReactNode;
   bottomNavbar?: React.ReactNode;
+  disableViewTransitions?: boolean;
 }) {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center">
+    <div className="flex min-h-full flex-1 flex-col items-center">
       {topNavbar && (
         <nav className="invisible w-full">
           <div className="flex w-full justify-center px-2">
@@ -20,27 +22,55 @@ export default function PageContainer({
       )}
       {topNavbar && (
         <nav
-          className="fixed top-0 z-50 w-full bg-content2/70 backdrop-blur-lg"
-          style={{
-            viewTransitionName: "jim-top-navbar",
-          }}
+          className="fixed left-0 right-0 top-0 z-50 bg-content2/70 backdrop-blur-lg"
+          style={
+            disableViewTransitions
+              ? undefined
+              : {
+                  viewTransitionName: "jim-top-navbar",
+                }
+          }
         >
-          <div className="flex w-full justify-center bg-gradient-to-b from-content2 to-transparent px-2">
-            <div className="w-full max-w-5xl">{topNavbar}</div>
+          <div
+            className="flex w-full justify-center bg-gradient-to-b from-content2 to-transparent px-2"
+            style={
+              disableViewTransitions
+                ? undefined
+                : {
+                    viewTransitionName: "jim-top-navbar-gradient",
+                  }
+            }
+          >
+            <div
+              className="w-full max-w-5xl"
+              style={
+                disableViewTransitions
+                  ? undefined
+                  : {
+                      viewTransitionName: "jim-top-navbar-wrapper",
+                    }
+              }
+            >
+              {topNavbar}
+            </div>
           </div>
         </nav>
       )}
 
       <main
         className="flex w-full flex-1 justify-center px-2 py-4"
-        style={{
-          viewTransitionName: "jim-main-content",
-        }}
+        style={
+          disableViewTransitions
+            ? undefined
+            : {
+                viewTransitionName: "jim-main-content",
+              }
+        }
       >
         <div className="flex w-full max-w-5xl flex-col gap-2">{children}</div>
       </main>
 
-      <div className="iphone-safe-inset">
+      <div className="iphone-safe-inset w-full">
         {bottomNavbar && (
           <nav className="invisible w-full">
             <div className="flex w-full justify-center">
@@ -52,9 +82,13 @@ export default function PageContainer({
       {bottomNavbar && (
         <nav
           className="fixed bottom-0 z-50 w-full rounded-t-xl bg-content2/70 backdrop-blur-lg"
-          style={{
-            viewTransitionName: "jim-bottom-navbar",
-          }}
+          style={
+            disableViewTransitions
+              ? undefined
+              : {
+                  viewTransitionName: "jim-bottom-navbar",
+                }
+          }
         >
           <div className="iphone-safe-inset flex w-full justify-center bg-gradient-to-t from-content2 to-transparent">
             <div className="w-full max-w-5xl">{bottomNavbar}</div>
