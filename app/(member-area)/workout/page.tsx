07ef919f6c5f13bf3f646f8c631e24e7392b6939
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2025-01-14 22:32:42 +0100

    added a "Starting workout" spinner modal

--- app/(member-area)/workout/page.tsx
+++ app/(member-area)/workout/page.tsx
@@ -19,11 +19,16 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
+  Modal,
+  ModalBody,
+  ModalContent,
+  Spinner,
 } from "@nextui-org/react";
 import { useMutation, useQuery } from "convex/react";
 import { Ellipsis, NotepadText, Plus, Search } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Page() {
   const routines = useQuery(api.routines.custom) ?? [];
@@ -31,81 +36,105 @@ export default function Page() {
   const createWorkout = useMutation(api.activeWorkouts.create);
   const { push } = useRouter();
 
+  const [isStarting, setIsStarting] = useState(false);
+
   const startWorkout = async (id?: Id<"routines">) => {
     try {
+      setIsStarting(true);
       const workoutId = await createWorkout({ id });
       push(`/workout/live/${workoutId}`);
-    } catch (error) {
-      console.log(error);
-      console.log("Failed to create workout");
+    } catch {
+      setIsStarting(false);
     }
   };
 
   return (
-    <PageContainer bottomNavbar={<BottomNavbar />}>
-      <TypographyH2>Quick Start</TypographyH2>
-      <Button size="lg" startContent={<Plus />} onPress={() => startWorkout()}>
-        Start Empty Workout
-      </Button>
-      <TypographyH2>Routines</TypographyH2>
-      <div className="grid grid-cols-2 gap-4">
-        <Button
-          as={Link}
-          href="/workout/create-routine"
-          size="lg"
-          startContent={<NotepadText />}
-        >
-          New Routine
-        </Button>
+    <>
+      <PageContainer bottomNavbar={<BottomNavbar />}>
+        <TypographyH2>Quick Start</TypographyH2>
         <Button
           size="lg"
-          startContent={<Search />}
-          className="under-construction"
+          startContent={<Plus />}
+          onPress={() => startWorkout()}
         >
-          Explore
+          Start Empty Workout
         </Button>
-      </div>
-      <TypographyH3>My Routines</TypographyH3>
-      {routines.map((r) => (
-        <Card key={r._id} fullWidth as="div">
-          <CardHeader className="items-start justify-between">
-            <div>
-              <TypographyH1>{r.name}</TypographyH1>
-              <TypographyH4>
-                {r.exercises.map((e) => e.name).join(", ")}
-              </TypographyH4>
+        <TypographyH2>Routines</TypographyH2>
+        <div className="grid grid-cols-2 gap-4">
+          <Button
+            as={Link}
+            href="/workout/create-routine"
+            size="lg"
+            startContent={<NotepadText />}
+          >
+            New Routine
+          </Button>
+          <Button
+            size="lg"
+            startContent={<Search />}
+            className="under-construction"
+          >
+            Explore
+          </Button>
+        </div>
+        <TypographyH3>My Routines</TypographyH3>
+        {routines.map((r) => (
+          <Card key={r._id} fullWidth as="div">
+            <CardHeader className="items-start justify-between">
+              <div>
+                <TypographyH1>{r.name}</TypographyH1>
+                <TypographyH4>
+                  {r.exercises.map((e) => e.name).join(", ")}
+                </TypographyH4>
+              </div>
+              <Dropdown placement="left">
+                <DropdownTrigger>
+                  <Button isIconOnly variant="light" size="md">
+                    <Ellipsis size={20} />
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu>
+                  <DropdownItem
+                    key="delete"
+                    color="danger"
+                    className="text-danger"
+                    onPress={() => {
+                      removeRoutine({ id: r._id });
+                    }}
+                  >
+                    Remove routine
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </CardHeader>
+            <CardFooter>
+              <Button
+                fullWidth
+                color="primary"
+                onPress={() => startWorkout(r._id)}
+              >
+                Start Routine
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </PageContainer>
+      <Modal
+        isOpen={isStarting}
+        isDismissable={false}
+        backdrop="blur"
+        placement="center"
+        hideCloseButton
+      >
+        <ModalContent>
+          <ModalBody className="p-4">
+            <div className="flex flex-row items-center justify-center gap-4">
+              <Spinner />
+              Starting workout...
             </div>
-            <Dropdown placement="left">
-              <DropdownTrigger>
-                <Button isIconOnly variant="light" size="md">
-                  <Ellipsis size={20} />
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu>
-                <DropdownItem
-                  key="delete"
-                  color="danger"
-                  className="text-danger"
-                  onPress={() => {
-                    removeRoutine({ id: r._id });
-                  }}
-                >
-                  Remove routine
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </CardHeader>
-          <CardFooter>
-            <Button
-              fullWidth
-              color="primary"
-              onPress={() => startWorkout(r._id)}
-            >
-              Start Routine
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
-    </PageContainer>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
