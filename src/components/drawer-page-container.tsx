9b452555448c12d02098aadd4b2bfac4716b5631
Hussein Taher <git@husseintaher.com>
2025-10-28 14:28:43 +0100

    formatting

--- src/components/drawer-page-container.tsx
+++ src/components/drawer-page-container.tsx
@@ -12,17 +12,17 @@ export default function DrawerPageContainer({
   return (
     <div className="flex h-full max-h-full min-h-full flex-1 flex-col items-center">
       {topNavbar && (
-        <nav className="flex w-full justify-center bg-content2 px-2">
+        <nav className="bg-content2 flex w-full justify-center px-2">
           <div className="w-full max-w-5xl">{topNavbar}</div>
         </nav>
       )}
 
-      <main className="flex w-full flex-1 justify-center overflow-y-auto overflow-x-hidden overscroll-x-none px-2 py-4">
+      <main className="flex w-full flex-1 justify-center overflow-x-hidden overflow-y-auto overscroll-x-none px-2 py-4">
         <div className="flex w-full max-w-5xl flex-col gap-2">{children}</div>
       </main>
 
       {bottomNavbar ? (
-        <nav className="iphone-safe-inset flex w-full justify-center bg-content2 px-2">
+        <nav className="iphone-safe-inset bg-content2 flex w-full justify-center px-2">
           <div className="w-full max-w-5xl">{bottomNavbar}</div>
         </nav>
       ) : (
--- src/components/exercise-list/exercises-context.ts
+++ src/components/exercise-list/exercises-context.ts
@@ -1,8 +1,10 @@
 import { UseExercisesReturn } from "@/components/exercise-list/use-exercises";
 import { createContext } from "@heroui/react-utils";
 
-export const [ExercisesProvider, useExercisesContext] = createContext<UseExercisesReturn>({
-  name: "ExercisesContext",
-  strict: true,
-  errorMessage: "useExercisesContext: `context` is undefined. Seems like you forgot to wrap component within <Exercises>",
-});
+export const [ExercisesProvider, useExercisesContext] =
+  createContext<UseExercisesReturn>({
+    name: "ExercisesContext",
+    strict: true,
+    errorMessage:
+      "useExercisesContext: `context` is undefined. Seems like you forgot to wrap component within <Exercises>",
+  });
