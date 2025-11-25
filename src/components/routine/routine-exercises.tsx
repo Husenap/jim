6728f3af2d799e1f2601c4730fd45b42552c4e06
Hussein Taher <git@husseintaher.com>
2025-11-25 12:09:45 +0100

    use motion Reorder instead of ReactSortable

--- src/components/routine/routine-exercises.tsx
+++ src/components/routine/routine-exercises.tsx
@@ -1,50 +1,52 @@
+"use client";
+
 import { useRoutineContext } from "@/components/routine/routine-context";
 import RoutineExerciseMenu from "@/components/routine/routine-exercise-menu";
 import GetSupersetColor from "@/utils/workout/superset";
 import { Avatar } from "@heroui/react";
 import { Dumbbell } from "lucide-react";
-import { ReactSortable } from "react-sortablejs";
+import { Reorder } from "motion/react";
 
 export default function RoutineExercises() {
   const { exercises, setExercises } = useRoutineContext();
 
   return (
-    <>
-      <ReactSortable
-        list={exercises}
-        setList={setExercises}
-        className="-mx-2 flex flex-col py-2"
-        animation={200}
-        chosenClass="bg-content2"
-      >
-        {exercises.map(({ exercise, id, superset }, index) => (
-          <div
-            key={id}
+    <Reorder.Group
+      values={exercises}
+      onReorder={setExercises}
+      axis="y"
+      className="-mx-2 flex flex-col py-2"
+    >
+      {exercises.map((item, index) => {
+        return (
+          <Reorder.Item
+            key={item.id}
+            value={item}
             className="transition-background flex flex-row justify-between p-2"
           >
             <div className="flex flex-row items-center gap-2">
               <Avatar
-                isBordered={superset !== undefined}
+                isBordered={item.superset !== undefined}
                 color={
-                  superset !== undefined
-                    ? GetSupersetColor(superset)
+                  item.superset !== undefined
+                    ? GetSupersetColor(item.superset)
                     : undefined
                 }
-                src={exercise.imageURL}
+                src={item.exercise.imageURL}
                 icon={<Dumbbell />}
                 size="md"
               />
               <div className="flex flex-col items-start">
-                <h2>{exercise.name}</h2>
+                <h2>{item.exercise.name}</h2>
                 <span className="text-default-400">
-                  {exercise.muscleGroups[0]?.muscleGroup}
+                  {item.exercise.muscleGroups[0]?.muscleGroup}
                 </span>
               </div>
             </div>
             <RoutineExerciseMenu exerciseIndex={index} />
-          </div>
-        ))}
-      </ReactSortable>
-    </>
+          </Reorder.Item>
+        );
+      })}
+    </Reorder.Group>
   );
 }
